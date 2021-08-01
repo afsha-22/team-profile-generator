6845258119b824129acff1f5181eb2587dd7cda9
Afsha <afsha@afterpaytouch.com>
2021-08-01 18:20:50 +1000

    BE ready

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const Manager = require('./lib/Manager')
 const Employee = require('./lib/Employee')
 const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
-const path = require("path");
+const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const generateTeam = require("./src/template.js")
 
 teamArray = [];
 
 function init() {
-    manager();
+    manager(); //Calling manager function as we always want to have a manager by default
 
     function createTeam() {
         inquirer.prompt([
@@ -31,7 +34,7 @@ function init() {
             } else if(member.newMember === "Intern") {
                 addIntern()
             } else {
-                console.log("Team created!")
+                buildHTML()
             }
         })
     }
@@ -100,41 +103,43 @@ function init() {
         })
       }
     
-      function addIntern() {
-        inquirer.prompt([
-          {
-            type: "input",
-            name: "name",
-            message: "What is the intern's name?"
-          },
-          {
-            type: "input",
-            name: "id",
-            message: "What is the intern's employee ID number?" 
-          },
-          {
-            type: "input",
-            name: "email",
-            message: "What is the intern's email address?"
-          },
-          {
-            type: "input",
-            name: "school",
-            message: "What school does the intern attend?"
-          }
-        ]).then(answers => {
-          const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-          teamArray.push(intern);
-          createTeam();
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-      }
+    function addIntern() {
+    inquirer.prompt([
+        {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+        },
+        {
+        type: "input",
+        name: "id",
+        message: "What is the intern's employee ID number?" 
+        },
+        {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?"
+        },
+        {
+        type: "input",
+        name: "school",
+        message: "What school does the intern attend?"
+        }
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        teamArray.push(intern);
+        createTeam();
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    }
+
+    function buildHTML () {
+    console.log("Team created!")
+    fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
+    }
 }
-
-
-
 
 
 
