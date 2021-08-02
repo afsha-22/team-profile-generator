08a747034eae794a39044a6a13055b2da1af2b79
Afsha <afsha@afterpaytouch.com>
2021-08-02 18:46:13 +1000

    Adding basic unit tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,11 +1,12 @@
-
-
 const Engineer = require('../lib/Engineer')
 
-describe("Verify Github account provided", () => {
-    it("githib", () => {
-        const testGithub = "alexgt";
-        const empInstance = new Engineer("alex", 1, "alex@email.com", testGithub)
-        expect(empInstance.testGithub).toBe(testGithub)
+describe("Verify Engineer related functions", () => {
+
+    describe("Verify Github account provided", () => {
+        it("Verify Github", () => {
+            const testGithub = "alexgt";
+            const empInstance = new Engineer("alex", 1, "alex@email.com", testGithub)
+            expect(empInstance.github).toBe(testGithub)
+        })
     })
 })
---
+++ tests/Manager.test.js
@@ -0,0 +1,11 @@
+const Manager = require('../lib/Manager')
+
+describe("Verify Manager Class", () => {
+    describe("Phone number", () => {
+        it("Phone", () => {
+            const testPhone = "0412345678"
+            const empInstance = new Manager("Maya", 3, "maya@email.com", testPhone)
+            expect(empInstance.officenumber).toBe(testPhone)
+        })
+    })
+})
